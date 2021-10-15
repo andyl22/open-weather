f6704fa06f31ec5cc580e521bfbf1fca8c0fdd3e
Andy Lau <landywl4@gmail.com>
2021-10-15 00:18:22 -0700

    Separating logic to separate modules and added geocoding to transform location inputs in order to use the one-call api

---
+++ src/createWeatherElement.js
@@ -0,0 +1,46 @@
+import moment from 'moment';
+
+async function assignWeatherElement(weather, iteration) {
+  const mainWeatherContainer = document.getElementsByClassName('entire-forecast')[0];
+  console.log(mainWeatherContainer.childNodes[iteration]);
+  const dayForecast = mainWeatherContainer.childNodes[iteration];
+  const icon = dayForecast.querySelector('.weather-icon');
+  const date = dayForecast.querySelector('.date');
+  const feelsLike = dayForecast.querySelector('.feels-like');
+  const tempText = dayForecast.querySelector('.temp p');
+  const condition = dayForecast.querySelector('.condition');
+  icon.src = `http://openweathermap.org/img/wn/${weather.icon}@2x.png`;
+  date.innerText = `${moment(weather.date).format('MMM Do YY')} \n ${moment(weather.date).format('LT')}`;
+  tempText.textContent = weather.temp;
+  feelsLike.innerText = `Feels Like\n ${weather.feelsLike} \n ${weather.wind} mph winds\n`;
+  condition.textContent = `${weather.condition} - ${weather.conditionDetail}`;
+}
+
+async function createWeatherElements() {
+  const mainWeatherContainer = document.getElementsByClassName('entire-forecast')[0];
+  const dayForecast = document.createElement('div');
+  const weatherDetails = document.createElement('div');
+  const temp = document.createElement('div');
+  const feelsLike = document.createElement('p');
+  const condition = document.createElement('p');
+  const date = document.createElement('p');
+  const icon = document.createElement('img');
+  const tempText = document.createElement('p');
+  dayForecast.className = 'day-forecast';
+  weatherDetails.className = 'weather-details';
+  icon.className = 'weather-icon';
+  date.className = 'date';
+  temp.className = 'temp';
+  feelsLike.className = 'feels-like';
+  condition.className = 'condition';
+  temp.appendChild(icon);
+  temp.appendChild(tempText);
+  weatherDetails.appendChild(date);
+  weatherDetails.appendChild(temp);
+  weatherDetails.appendChild(feelsLike);
+  weatherDetails.appendChild(condition);
+  dayForecast.appendChild(weatherDetails);
+  mainWeatherContainer.appendChild(dayForecast);
+}
+
+export { assignWeatherElement, createWeatherElements };
---
+++ src/getWeatherForecast.js
@@ -0,0 +1,53 @@
+import { createWeatherElements, assignWeatherElement } from './createWeatherElement';
+
+const positionStackAPIKey = 'b3950d120c0d2d41174eb990321f4389';
+const openWeatherAPIKey = 'facef8942e15e4523244c4fecc91b001';
+
+async function getLatLong(locationQuery) {
+  let response = await fetch(`http://api.positionstack.com/v1/forward?access_key=${positionStackAPIKey}&query=${locationQuery}`);
+  try {
+    response = await response.json();
+    return [response.data[0].latitude, response.data[0].longitude];
+  } catch (error) {
+    throw new Error(400);
+  }
+}
+
+async function getWeatherForecast(latLong) {
+  const [lat, long] = latLong;
+  let response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=minutely&appid=${openWeatherAPIKey}`);
+  try {
+    response = response.json();
+    return response;
+  } catch (error) {
+    throw new Error(400);
+  }
+}
+
+async function weatherParseObj(weatherObj) {
+  const weather = {
+    temp: weatherObj.temp,
+    feelsLike: weatherObj.feels_like,
+    condition: weatherObj.weather[0].main,
+    conditionDetail: weatherObj.weather[0].description,
+    date: new Date(weatherObj.dt * 1000),
+    wind: weatherObj.wind_speed,
+    icon: weatherObj.weather[0].icon,
+  };
+  return weather;
+}
+
+async function getOpenWeatherForCity() {
+  return getLatLong('San Francisco, California')
+    .then((latLong) => getWeatherForecast(latLong))
+    .then((weatherParent) => {
+      for (let i = 1; i < weatherParent.hourly.length; i += 1) {
+        weatherParseObj(weatherParent.hourly[i])
+          .then(createWeatherElements())
+          .then((parsedWeatherObj) => assignWeatherElement(parsedWeatherObj, i));
+      }
+    })
+    .catch((error) => alert(error.message));
+}
+
+export default getOpenWeatherForCity;
