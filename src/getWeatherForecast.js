fd7b7b0ae365e4531782e854db6c18c674f94be5
Andy Lau <landywl4@gmail.com>
2021-10-15 00:22:16 -0700

    Added cors mode to fetch requests

--- src/getWeatherForecast.js
+++ src/getWeatherForecast.js
@@ -4,7 +4,7 @@ const positionStackAPIKey = 'b3950d120c0d2d41174eb990321f4389';
 const openWeatherAPIKey = 'facef8942e15e4523244c4fecc91b001';
 
 async function getLatLong(locationQuery) {
-  let response = await fetch(`http://api.positionstack.com/v1/forward?access_key=${positionStackAPIKey}&query=${locationQuery}`);
+  let response = await fetch(`http://api.positionstack.com/v1/forward?access_key=${positionStackAPIKey}&query=${locationQuery}`, { mode: 'cors' });
   try {
     response = await response.json();
     return [response.data[0].latitude, response.data[0].longitude];
@@ -15,7 +15,7 @@ async function getLatLong(locationQuery) {
 
 async function getWeatherForecast(latLong) {
   const [lat, long] = latLong;
-  let response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=minutely&appid=${openWeatherAPIKey}`);
+  let response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=minutely&appid=${openWeatherAPIKey}`, { mode: 'cors' });
   try {
     response = response.json();
     return response;
